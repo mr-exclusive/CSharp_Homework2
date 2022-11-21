1220d25c2509a4d104df7dd32f3ce817ddc6a708
alexey.polyanichko <alexey.polyanichko@gmail.com>
2022-11-21 20:16:04 +0200

    Problem 10

---
+++ Problem10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+var rnd = new Random();
+int randNumber = rnd.Next(100, 999); 
+
+Console.WriteLine($"{randNumber} -> {randNumber.ToString()[1]}");
